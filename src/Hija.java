f6dfb1bcf362e88753639ecf5a024108f21c8c94
martinmaresc <martinmarescr@gmail.com>
2023-05-17 15:39:07 -0600

    Ejemplo de Override entre clases

--- src/Hija.java
+++ src/Hija.java
@@ -1,6 +1,6 @@
 public class Hija extends Padre{
     @Override
     public void caminar() {
-        System.out.println("La hija camina");
+        System.out.println("La hija camina sola");
     }
 }
